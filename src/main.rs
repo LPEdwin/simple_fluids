67ecbe0a5678f8ee19f90794e7e6c696839ee6e8
NB <n.baumgaertner@hotmail.com>
2025-07-09 19:40:30 +0200

    cargo init

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,7 @@
-use std::ops::Add;
-
-#[derive(Debug, Clone, Copy)]
-struct Vector3 {
-    x: f64,
-    y: f64,
-    z: f64,
-}
-
-impl Add for Vector3 {
-    type Output = Vector3;
-
-    fn add(self, other: Vector3) -> Vector3 {
-        Vector3 {
-            x: self.x + other.x,
-            y: self.y + other.y,
-            z: self.z + other.z,
-        }
-    }
-}
+mod estimate_pi;
+mod vec3;
 
 fn main() {
-    let v1: Vector3 = Vector3 {
-        x: 3.14,
-        y: 55.0,
-        z: 0.55,
-    };
-    let v2: Vector3 = Vector3 {
-        x: 0.0,
-        y: 1000.0,
-        z: 0.55,
-    };
-
-    println!("{:?}", v1 + v2);
+    estimate_pi::run_brute_force(1_000_000_00);
+    estimate_pi::run_stratified(1_000_0);
 }
