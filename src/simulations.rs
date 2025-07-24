84e4002906d7c5a7c81fe22101b37738a4e8cc98
NB <n.baumgaertner@hotmail.com>
2025-07-24 03:06:02 +0200

    added brownian sim

--- src/simulations.rs
+++ src/simulations.rs
@@ -6,9 +6,10 @@ use crate::{
     vector2::Vector2,
 };
 
-pub fn collision_sim() -> ImpulsSimulation {
-    let color = Color::new(0.0, 0.8667, 0.8353, 1.0);
+const GREEN: Color = Color::new(0.0, 0.8667, 0.8353, 1.0);
+const RED: Color = Color::new(0.9254, 0.0745, 0.2745, 1.0);
 
+pub fn collision_sim() -> ImpulsSimulation {
     const EPS: f64 = 1e-8;
     const RADIUS: f64 = 0.02;
 
@@ -26,7 +27,7 @@ pub fn collision_sim() -> ImpulsSimulation {
             position: Vector2::random_in_rectangle(spawn_bounds_x.clone(), spawn_bounds_y.clone()),
             velocity: Vector2::random_in_disk() * 0.5,
             radius: RADIUS,
-            color: color,
+            color: GREEN,
         });
     }
 
@@ -43,9 +44,6 @@ pub fn collision_sim() -> ImpulsSimulation {
 }
 
 pub fn mixing_sim() -> ImpulsSimulation {
-    let color1 = Color::new(0.0, 0.8667, 0.8353, 1.0);
-    let color2 = Color::new(0.9254, 0.0745, 0.2745, 1.0);
-
     const EPS: f64 = 1e-8;
     const RADIUS: f64 = 0.01;
     const COUNT: usize = 1000;
@@ -65,7 +63,7 @@ pub fn mixing_sim() -> ImpulsSimulation {
             position: Vector2::random_in_rectangle(spawn_bounds_x.clone(), spawn_bounds_y1.clone()),
             velocity: Vector2::random_in_disk() * 0.5,
             radius: RADIUS,
-            color: color1,
+            color: GREEN,
         });
     }
 
@@ -75,7 +73,7 @@ pub fn mixing_sim() -> ImpulsSimulation {
             position: Vector2::random_in_rectangle(spawn_bounds_x.clone(), spawn_bounds_y2.clone()),
             velocity: Vector2::random_in_disk() * 0.5,
             radius: RADIUS,
-            color: color2,
+            color: RED,
         });
     }
 
@@ -86,6 +84,53 @@ pub fn mixing_sim() -> ImpulsSimulation {
         view: boundary,
         boundary,
         gravity: Vector2 { x: 0.0, y: -0.0 },
+        restitution: 1.0,
+        ..Default::default()
+    }
+}
+
+pub fn brownian_motion_sim() -> ImpulsSimulation {
+    const EPS: f64 = 1e-8;
+    const RADIUS: f64 = 0.005;
+    const MASS: f64 = 1.0;
+
+    let boundary = Rectangle {
+        min: Vector2 { x: 0.0, y: 0.0 },
+        max: Vector2 { x: 2.0, y: 1.0 },
+    };
+    let spawn_bounds_x = RADIUS + boundary.min.x + EPS..boundary.max.x - RADIUS - EPS;
+    let spawn_bounds_y = RADIUS + boundary.min.y + EPS..boundary.max.y - RADIUS - EPS;
+
+    const KB: f64 = 1.0; // Boltzmann constant, normalized
+    const T: f64 = 1.0; // arbitrary temperature
+    let sigma = (KB * T / MASS).sqrt();
+
+    let mut particles = Vec::new();
+    for _ in 0..1000 {
+        particles.push(Particle {
+            mass: MASS,
+            position: Vector2::random_in_rectangle(spawn_bounds_x.clone(), spawn_bounds_y.clone()),
+            velocity: Vector2::random_gaussian(0.0, sigma),
+            radius: RADIUS,
+            color: GREEN,
+        });
+    }
+
+    particles.push(Particle {
+        mass: MASS * 100.0,
+        position: Vector2::random_in_rectangle(spawn_bounds_x.clone(), spawn_bounds_y.clone()),
+        velocity: Vector2::ZERO,
+        radius: RADIUS * 10.0,
+        color: RED,
+    });
+
+    ImpulsSimulation {
+        window_width: 500.0,
+        window_height: 500.0,
+        particles,
+        view: boundary,
+        boundary,
+        gravity: Vector2::ZERO,
         restitution: 1.0,
         ..Default::default()
     }
