7bf004c87483a3c91afaf8392eb644e628169cc1
NB <n.baumgaertner@hotmail.com>
2025-09-06 00:47:05 +0200

    added operators

--- src/vector2.rs
+++ src/vector2.rs
@@ -49,40 +49,29 @@ impl_op!(Sub, sub, -);
 impl_op!(Mul, mul, *);
 impl_op!(Div, div, /);
 
-impl std::ops::Neg for Vector2 {
-    type Output = Vector2;
-
-    fn neg(self) -> Self::Output {
-        Vector2 {
-            x: -self.x,
-            y: -self.y,
+macro_rules! impl_op_scalar {
+    ($trait:ident, $func:ident, $op:tt) => {
+        impl std::ops::$trait<f64> for Vector2 {
+            type Output = Vector2;
+            #[inline]
+            fn $func(self, rhs: f64) -> Vector2 {
+                Vector2 { x: self.x $op rhs, y: self.y $op rhs }
+            }
         }
-    }
+
+        impl std::ops::$trait<Vector2> for f64 {
+            type Output = Vector2;
+            #[inline]
+            fn $func(self, rhs: Vector2) -> Vector2 {
+                Vector2 { x: self $op rhs.x, y: self $op rhs.y }
+            }
+        }
+    };
 }
 
-impl std::ops::Mul<Vector2> for f64 {
-    type Output = Vector2;
-
-    #[inline]
-    fn mul(self, rhs: Vector2) -> Vector2 {
-        Vector2 {
-            x: self * rhs.x,
-            y: self * rhs.y,
-        }
-    }
-}
-
-impl std::ops::Mul<f64> for Vector2 {
-    type Output = Vector2;
-
-    #[inline]
-    fn mul(self, rhs: f64) -> Vector2 {
-        Vector2 {
-            x: self.x * rhs,
-            y: self.y * rhs,
-        }
-    }
-}
+impl_op_scalar!(Add, add, +);
+impl_op_scalar!(Sub, sub, -);
+impl_op_scalar!(Mul, mul, *);
 
 impl std::ops::Div<f64> for Vector2 {
     type Output = Vector2;
@@ -94,6 +83,17 @@ impl std::ops::Div<f64> for Vector2 {
                 x: self.x,
                 y: self.y,
             }
+    }
+}
+
+impl std::ops::Neg for Vector2 {
+    type Output = Vector2;
+
+    fn neg(self) -> Self::Output {
+        Vector2 {
+            x: -self.x,
+            y: -self.y,
+        }
     }
 }
 
@@ -136,6 +136,16 @@ impl Vector2 {
             Vector2 {
                 x: rng.random_range(x_range),
                 y: rng.random_range(y_range),
+            }
+        })
+    }
+
+    pub fn random_min_max(min: Vector2, max: Vector2) -> Vector2 {
+        RNG.with(|cell| {
+            let rng = unsafe { &mut *cell.get() };
+            Vector2 {
+                x: rng.random_range(min.x..max.x),
+                y: rng.random_range(min.y..max.y),
             }
         })
     }
